4dd64520ef5b9e3329de8fae4b5ecadfe2f9f673
Mohd. Sameer <66036809+Sameer-Shahzada@users.noreply.github.com>
2026-06-02 19:47:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll(".code");
+
+// Focus first input
+codes[0].focus();
+
+codes.forEach((code, idx) => {
+  code.addEventListener("input", (e) => {
+    if (e.target.value.length === 1 && idx < codes.length - 1) {
+      codes[idx + 1].focus();
+    }
+  });
+
+  code.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+      code.value = "";
+
+      if (idx > 0) {
+        codes[idx - 1].focus();
+      }
+    }
+  });
+});
